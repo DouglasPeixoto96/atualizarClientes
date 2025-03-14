8592d33acb7ee720c6012b76ee82bcb964bd14c4
Douglas Peixoto <douglaspeixoto18@gmail.com>
2025-03-14 09:21:25 -0300

    app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Servidor rodando!"
+
+@app.route('/update-db')
+def update_db():
+    subprocess.run(["python3", "seu_script.py"])
+    return "Atualização concluída!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=10000)
